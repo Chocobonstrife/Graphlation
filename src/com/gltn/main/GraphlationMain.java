ba9084dd5c4ab12a09acff011411448e2ac94941
Eric Dalrymple <chocobonstrife@hotmail.com>
2014-10-18 16:31:33 -0400

    - Added main class to be the entry point.

---
+++ src/com/gltn/main/GraphlationMain.java
@@ -0,0 +1,39 @@
+package com.gltn.main;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class GraphlationMain
+extends JFrame
+{
+
+  private static final long serialVersionUID = 191763243272774216L;
+
+  public GraphlationMain() throws HeadlessException
+  {
+    
+  }
+
+  public GraphlationMain( GraphicsConfiguration arg0 )
+  {
+    super( arg0 );
+  }
+
+  public GraphlationMain( String arg0 ) throws HeadlessException
+  {
+    super( arg0 );
+  }
+
+  public GraphlationMain( String arg0, GraphicsConfiguration arg1 )
+  {
+    super( arg0, arg1 );
+  }
+
+  public static void main( String[] args )
+  {
+    
+  }
+
+}
